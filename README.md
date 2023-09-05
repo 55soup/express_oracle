67205e2fd7bb29bab7541a3df14d1282826b1fa9
55soup <w2118@e-mirim.hs.kr>
2023-09-05 10:05:10 +0900

    주석 추가

---
+++ README.md
@@ -0,0 +1,22 @@
+# express, oracledb 연동
+
+### 1. npm 모듈을 다운받습니다.
+```
+npm install
+```
+
+### 2. env 파일에 유저, 비밀번호, connect string을 작성합니다.
+
+### 3. node 서버를 실행합니다.
+```
+node app
+
+// nodemon이 있는 경우
+nodemon app
+```
+
+> ### oracledb 패키지 버전
+
+로컬 DB 연동: **oracledb 5버전**
+
+aws rds DB 연동: **oracledb 6버전**
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// 로컬 DB 연동: oracledb 5버전
+// aws rds DB 연동: oracledb 6버전
 const express = require('express')
-const dbconfig = require("./dbconfig");
+const dbconfig = require("./dbconfig"); // db 세팅
+require("dotenv").config();
 const app = express()
+const SERVER_PORT = 3001;
 
-const server = app.listen(3001, () => {
+const server = app.listen(SERVER_PORT, () => {
     console.log('server start, port 3001')
 })
 
@@ -19,7 +23,7 @@ async function getSelect(request, response) {
         connection = await oracledb.getConnection(dbconfig);
         const result = await connection.execute(
             `SELECT *
-            FROM gpt_questions`, //gpt_questions
+            FROM employees`, //TEST TABLE -- 테이블 이름은 필요에 따라 수정.
             // [1], // num의 값 전달
         )
 
